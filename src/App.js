03062f1d896f8485817c94d32f286af2184e1532
Brian-CodeNation <brian.harkins@wearecodenation.com>
2023-12-04 21:00:41 +0000

    Added files n stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Modal from "./Modal"
+import './App.css'
 
-function App() {
+const App = () => {
+  const [errorMsg, setErrorMsg] = useState('');
+  const [characters, setCharacters] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState({})
+  const [showModal, setShowModal] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setErrorMsg('')
+        const response = await fetch('https://api.sampleapis.com/rickandmorty/characters');
+        
+        if(!response.ok){
+          throw new Error(response.statusText)
+        }
+
+        const data = await response.json();
+        setCharacters(data)
+        setLoading(false)
+      } catch (error) {
+        setErrorMsg("Oops something went wrong...")
+        console.log(error.message)
+      }
+    }
+    fetchData()
+  }, [])
+
+  const handleClick = (character) => {
+    setSelectedProduct(character);
+    setShowModal(true);
+  }
+
+  if(errorMsg !== ''){
+    return <h1>{errorMsg}</h1>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="pageWrapper">
+      <h1>My API Database</h1>
+      <code>API endpoint: "https://api.sampleapis.com/rickandmorty/characters"</code>
+
+      {/* map through data from API that is stored in the state */}
+      <div className="imageWrapper">
+        {loading && (
+          <h2>Loading...</h2>
+        )}
+        {characters.map((character, index) => {
+          return (
+            <div className="center">
+              <img key={index} src={character.image} alt="Rick n Morty characters" onClick={() => handleClick(character)}/>
+              <h4><b>{character.name}</b></h4>
+              
+            </div>
+          )
+        })}
+      </div>
+      
+      {showModal && <Modal closeModal={setShowModal} product={selectedProduct}/>}
     </div>
   );
 }
---
+++ src/Modal.js
@@ -0,0 +1,34 @@
+import { useRef } from "react";
+
+const Modal = (props) => {
+    const modalRef = useRef();
+
+    const handleClick = (e) => {
+        if(modalRef.current === e.target){
+            props.closeModal(false)
+        }
+    }
+    const handleCloseClick = (e) => {
+        props.closeModal(false)
+    }
+
+    return (
+        <div id="modalBackground" ref={modalRef} onClick={handleClick}>
+            <div id="modalBox">
+                <p id="closeIcon" onClick={handleCloseClick}>x</p>
+                <div id="leftSection">
+                    <img src={props.product.image} alt="digimon"/>
+                </div>
+                <div id="rightSecion">
+                    <h5>{props.product.name}</h5>
+                    <h1>{props.product.species}</h1>  
+                    <h2>{props.product.status}</h2>    
+                    <h4>{props.product.gender}</h4>    
+                    <h2>{props.product.origin}</h2>                   
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Modal;
